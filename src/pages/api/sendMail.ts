73a0446d78444f856188ceff5c69d5a306791a6e
Ketan Saraf <saraf.ketan1108@gmail.com>
2023-05-20 01:32:12 +0530

    feat: mail

---
+++ src/pages/api/sendMail.ts
@@ -0,0 +1,34 @@
+// pages/api/sendEmail.js
+
+import { transporter, mailOptions } from "@/utils/nodemailer";
+import { NextApiRequest, NextApiResponse } from "next";
+import { type SendMailOptions } from "nodemailer";
+
+type Request = NextApiRequest & {
+  body: {
+    subject: string;
+    text: string;
+  };
+};
+
+export default async function handler(req: Request, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
+
+  const mail: SendMailOptions = {
+    ...mailOptions,
+    subject: req.body.subject,
+    text: req.body.text,
+  };
+
+  try {
+    await transporter.sendMail(mail);
+    res.status(200).json({ message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res
+      .status(500)
+      .json({ message: "An error occurred while sending the email" });
+  }
+}
