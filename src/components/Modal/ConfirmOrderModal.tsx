0251864cfe8941f8bf958d811cf7b12f801f2df2
Ketan Saraf <saraf.ketan1108@gmail.com>
2023-05-27 14:15:17 +0530

    feat: confirm modal

--- src/components/Modal/ConfirmOrderModal.tsx
+++ src/components/Modal/ConfirmOrderModal.tsx
@@ -147,7 +147,7 @@ const ConfirmOrderModal = ({ open, setOpen, data }: Props) => {
             }
           }}
         >
-          <div className="w-full sm:w-96 h-auto max-h-screen sm:max-h-[800px] min-h-[100px] bg-white sm:rounded-lg shadow-lg gap-3 flex flex-col overflow-y-auto py-4">
+          <div className="w-full sm:w-96 h-auto max-h-screen sm:max-h-[1000px] min-h-[100px] bg-white sm:rounded-lg shadow-lg gap-3 flex flex-col overflow-y-auto py-4">
             <div className="flex flex-row justify-end items-center text-base font-semibold leading-7 text-gray-900 bg-gray-100 px-4 py-2">
               <h2>Place Order</h2>
               <span className="ml-auto" onClick={() => setOpen(false)}>
@@ -195,7 +195,6 @@ const ConfirmOrderModal = ({ open, setOpen, data }: Props) => {
                 data={getClientNames}
               />
             </div>
-
             <div className="px-4">
               <label className="block text-sm font-medium leading-6 text-gray-900 mb-2">
                 {"Shipping Client Name"}
@@ -217,52 +216,47 @@ const ConfirmOrderModal = ({ open, setOpen, data }: Props) => {
                 data={getClientNames}
               />
             </div>
+            <div className="px-4">
+              <label className="block text-sm font-medium leading-6 text-gray-900 mb-2">
+                {"Client Address"}
+              </label>
+              <Autocomplete
+                id="clientAddress"
+                value={clientAddress}
+                onChange={(value) => setClientAddress(value)}
+                limit={50}
+                maxDropdownHeight={300}
+                styles={{
+                  input: {
+                    border: "none",
+                  },
+                }}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 overflow-clip p-1"
+                placeholder="Pick one"
+                data={getClientAddress(clientName)}
+              />
+            </div>
+            <div className="px-4">
+              <label className="block text-sm font-medium leading-6 text-gray-900 mb-2">
+                {"Shipping Client Address"}
+              </label>
+              <Autocomplete
+                id="shippingClientAddress"
+                value={shippingClientAddress}
+                limit={50}
+                maxDropdownHeight={300}
+                onChange={(value) => setShippingClientAddress(value)}
+                styles={{
+                  input: {
+                    border: "none",
+                  },
+                }}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 overflow-clip p-1"
+                placeholder="Pick one"
+                data={getClientAddress(shippingClientName)}
+              />
+            </div>
 
-            {clientName.length !== 0 && (
-              <div className="px-4">
-                <label className="block text-sm font-medium leading-6 text-gray-900 mb-2">
-                  {"Client Address"}
-                </label>
-                <Autocomplete
-                  id="clientAddress"
-                  value={clientAddress}
-                  onChange={(value) => setClientAddress(value)}
-                  limit={50}
-                  maxDropdownHeight={300}
-                  styles={{
-                    input: {
-                      border: "none",
-                    },
-                  }}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 overflow-clip p-1"
-                  placeholder="Pick one"
-                  data={getClientAddress(clientName)}
-                />
-              </div>
-            )}
-
-            {shippingClientName.length !== 0 && (
-              <div className="px-4">
-                <label className="block text-sm font-medium leading-6 text-gray-900 mb-2">
-                  {"Shipping Client Address"}
-                </label>
-                <Autocomplete
-                  id="shippingClientAddress"
-                  value={shippingClientAddress}
-                  limit={50}
-                  maxDropdownHeight={300}
-                  onChange={(value) => setShippingClientAddress(value)}
-                  styles={{
-                    input: {
-                      border: "none",
-                    },
-                  }}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 overflow-clip p-1"
-                  placeholder="Pick one"
-                  data={getClientAddress(shippingClientName)}
-                />
-              </div>
-            )}
             <button
               type="button"
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mx-4"
