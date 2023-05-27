a1b22d03348ecbfc5056803aff3ec44843bb98ba
Ketan Saraf <saraf.ketan1108@gmail.com>
2023-05-27 14:31:01 +0530

    feat: layout design

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -20,13 +20,18 @@ type Props = {
 
 export default function Layout({ children }: Props) {
   const router = useRouter();
-  const userNavigation = [{ name: "Sign out", action: logoutHandler }];
+  const userNavigation = [
+    { name: "Sign out", action: logoutHandler },
+    {
+      name: "Settings",
+      action: () => router.push("/settings"),
+    },
+  ];
   const navigation: Navigation[] = [
     { name: "Stocks", href: "/stocks" },
     { name: "Orders", href: "/orders" },
     { name: "Clients", href: "/clients" },
     { name: "Upload", href: "/upload" },
-    { name: "Settings", href: "/settings" },
   ];
   const [currentTab, setCurrentTab] = useState<string>("");
   const user = useAppSelector((state) => state.user);
@@ -110,7 +115,13 @@ export default function Layout({ children }: Props) {
                       <div>
                         <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                           <span className="sr-only">Open user menu</span>
-                          <div className="h-8 w-8 rounded-full bg-indigo-400" />
+                          <div className="h-8 w-8 rounded-full bg-indigo-400 flex justify-center items-center">
+                            {user.name
+                              .split(" ")
+                              .slice(0, 2)
+                              .map((name) => name.charAt(0).toLocaleUpperCase())
+                              .join("")}
+                          </div>
                         </Menu.Button>
                       </div>
                       <Transition
@@ -123,6 +134,18 @@ export default function Layout({ children }: Props) {
                         leaveTo="transform opacity-0 scale-95"
                       >
                         <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <Menu.Item
+                            as="p"
+                            className="px-4 pt-2 pb-1 text-xs text-gray-900 font-medium"
+                          >
+                            {user.name}
+                          </Menu.Item>
+                          <Menu.Item
+                            as="p"
+                            className="px-4 pt-0 pb-2 text-xs text-gray-400"
+                          >
+                            {user.email}
+                          </Menu.Item>
                           {userNavigation.map((item) => (
                             <Menu.Item key={item.name}>
                               {({ active }) => (
@@ -186,7 +209,13 @@ export default function Layout({ children }: Props) {
                 <div className="border-t border-gray-200 pb-3 pt-4">
                   <div className="flex items-center px-4">
                     <div className="flex-shrink-0">
-                      <div className="h-10 w-10 rounded-full bg-indigo-400" />
+                      <div className="h-8 w-8 rounded-full bg-indigo-400 flex justify-center items-center">
+                        {user.name
+                          .split(" ")
+                          .slice(0, 2)
+                          .map((name) => name.charAt(0).toLocaleUpperCase())
+                          .join("")}
+                      </div>
                     </div>
                     <div className="ml-3">
                       <div className="text-base font-medium text-gray-800">
@@ -196,13 +225,13 @@ export default function Layout({ children }: Props) {
                         {user.email}
                       </div>
                     </div>
-                    <button
+                    {/* <button
                       type="button"
                       className="ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                     >
                       <span className="sr-only">View notifications</span>
                       <BellIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
+                    </button> */}
                   </div>
                   <div className="mt-3 space-y-1">
                     {userNavigation.map((item) => (
